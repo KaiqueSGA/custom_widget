fcc566f8b47941b89a91cad741d264787e709375
KaiqueYudji <yudjikaique@gmail.com>
2023-04-19 14:37:57 -0300

    developing new funcionality

--- project/src/widgets/device_variables/device_variables.js
+++ project/src/widgets/device_variables/device_variables.js
@@ -1,5 +1,5 @@
 import './device_variables.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const { tago_device } = require('../../classes/device/device.js');
 const { location_apis } = require('../../classes/location/location.js')
@@ -19,18 +19,32 @@ function Device_variables(){
 
         let device_id = widget.display.variables[0].origin.id;
         let request = await device_methods.get_device_variables(device_id); 
-        set_device_variables(request.filter(obj => obj.variable === "esn"));
+        set_device_variables(request); 
     })
     window.TagoIO.ready(); 
+  
 
+  const oi = async() => {
+        let request = await device_methods.get_device_variables("6419c2dea997d700090c60ae"); 
+        set_device_variables(request); 
+  }
 
-   console.log("UÉ");
-   console.log("PQ N ATUALIZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
+    useEffect(() => {
+    oi()
+    },[])
     
-    
+   
 
-   console.log(window.location.protocol + "//" + window.location.host + "/" + window.location.pathname);
+   function add_0_to_left(value){
+
+      if( String(value).length === 1 ){
+        return "0" + value;
+      }else{
+        return value;
+      }
+
+   }
+
 
 
     return(
@@ -49,37 +63,41 @@ function Device_variables(){
                         <table border="1" className='variable_tables'>
                             <tr>
                                 <td>Origin</td>
+                                <td>Date and Time</td>
                                 <td>Coordinates</td>
                                   
                             </tr>
-                            {device_variables.map(data => {
+                            {device_variables.map(data => { 
 
                                 setTimeout(() => {// This delay is necessary because firt i need to create an element with the corresponding tag and after i catch the element through of id 
                                     const element_id = data.id;
                                     const scope = data; 
 
-                                    document.getElementById(data.id).addEventListener('change', async function(){
+                                    document.getElementById(data.id).addEventListener('change', async function(){console.log(this.value)
                                       
                                         if(this.value === "MAC"){ 
                                           let element_to_insert_the_new_value = document.getElementsByClassName(element_id)[0];
                                           let element_to_insert_the_address = document.getElementsByClassName(element_id)[1]; 
 
                                           let mac_coordinates = await location_funcs.get_coordinates_through_mac_datas(["mac0", "mac1", "mac2"],scope); 
-                                          let address = await location_funcs.get_address_through_coordinates(mac_coordinates.lat, mac_coordinates.lng);
+                                          let address = mac_coordinates.lat === 0 || mac_coordinates.lng === 0 ? "invalid data" :await location_funcs.get_address_through_coordinates(mac_coordinates.lat, mac_coordinates.lng);
                                          
                                           element_to_insert_the_new_value.innerText = `${mac_coordinates.lat},${mac_coordinates.lng}`;
                                           element_to_insert_the_address.innerText = address;
                                         }
+
                                         else if(this.value === "LBS"){
                                           let element_to_insert_the_new_value = document.getElementsByClassName(element_id)[0];
                                           let element_to_insert_the_address = document.getElementsByClassName(element_id)[1];//Position 1 because i yhave 2 elements with the same tags, the second element will render the address
 
-                                          let lbs_coordinates = await location_funcs.get_coordinates_through_lbs_datas(["lbs0", "lbs1", "lbs2"],scope, scope.metadata.lbs_mode === "LTE" ?"lte" :"gsm" ); 
-                                          let address = await location_funcs.get_address_through_coordinates(lbs_coordinates.lat, lbs_coordinates.lng);
+                                          console.log(element_to_insert_the_new_value, element_to_insert_the_address)
+                                          let lbs_coordinates = await location_funcs.get_coordinates_through_lbs_datas(["lbs0", "lbs1", "lbs2"],scope, scope.metadata.lbs_mode === "LTE" ?"lte" :"gsm" ); console.log(lbs_coordinates)
+                                          let address =  lbs_coordinates.lat === 0 || lbs_coordinates.lng === 0 ? "invalid data" :await location_funcs.get_address_through_coordinates(lbs_coordinates.lat, lbs_coordinates.lng);console.log(address)
                                          
                                           element_to_insert_the_new_value.innerText = `${lbs_coordinates.lat},${lbs_coordinates.lng}`; 
                                           element_to_insert_the_address.innerText = address;
                                         }
+
                                         else{ 
                                           let element_to_insert_the_coordinates = document.getElementsByClassName(element_id)[0];
                                           let element_to_insert_the_address = document.getElementsByClassName(element_id)[1]; 
@@ -99,6 +117,8 @@ function Device_variables(){
                               return( 
                                 <tr>
                                     <td className='data_variable'>{data.metadata.origin}</td>
+
+                                     <td>{`${ add_0_to_left((data.time.getMonth()) + 1)  }/${ add_0_to_left(data.time.getDate()) }/${ add_0_to_left(data.time.getFullYear()) } ${ add_0_to_left(data.time.getHours()) }:${ add_0_to_left(data.time.getMinutes()) }:${ add_0_to_left(data.time.getSeconds()) }`}</td>  
 
                                     <td className='b'>
                                         <select id={data.id} className ="coordinate-types">
